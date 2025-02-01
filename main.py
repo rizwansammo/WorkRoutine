ad884077a0a7e069e3734b43d3f4a65c6049acfe
Rizwan Sammo <mrizwan.sammo@gmail.com>
2025-02-01 15:07:19 +0600

    main.py Updated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-# todo_list.py
-
-tasks = []
+# main.py
+from todo_manager import view_tasks, add_task, mark_task_done
 
 def show_menu():
     print("\nTo-Do List Menu:")
@@ -8,31 +7,6 @@ def show_menu():
     print("2. Add Task")
     print("3. Mark Task as Done")
     print("4. Exit")
-
-def view_tasks():
-    if not tasks:
-        print("No tasks found!")
-    else:
-        print("\nTasks:")
-        for index, task in enumerate(tasks, start=1):
-            print(f"{index}. {task}")
-
-def add_task():
-    task = input("Enter a new task: ")
-    tasks.append(task)
-    print(f"Task '{task}' added!")
-
-def mark_task_done():
-    view_tasks()
-    try:
-        task_number = int(input("Enter the task number to mark as done: "))
-        if 1 <= task_number <= len(tasks):
-            removed_task = tasks.pop(task_number - 1)
-            print(f"Task '{removed_task}' marked as done!")
-        else:
-            print("Invalid task number!")
-    except ValueError:
-        print("Please enter a valid number!")
 
 def main():
     while True:
